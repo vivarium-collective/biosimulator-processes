3ac3446454616b4438860072d4d355c722b2d2b2
alexPatrie <alexanderpatrie@gmail.com>
2024-02-21 11:23:53 -0500

    updated run docker

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -2,9 +2,8 @@
 yes | docker system prune
 docker buildx create --name biosimbuilder --use
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64 -t biosimulators/biosimulator-processes:0.0.1 . \
-  && docker run --platform linux/amd64 -it -p 8888:8888 biosimulator-processes
-
+docker buildx build --platform linux/amd64 -t ghcr.io/biosimulators/biosimulator-processes:0.0.1 . \
+  && docker run --platform linux/amd64 -it -p 8888:8888 biosimulators/biosimulator-processes:0.0.1
 
 
 # FIX REGISTRATION ON INIT
