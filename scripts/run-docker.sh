544e3414594d99eb26cc1c938c345338c030fdf4
alexPatrie <alexanderpatrie@gmail.com>
2024-02-21 10:58:30 -0500

    added main notebook and updated docker runner

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -2,7 +2,7 @@
 yes | docker system prune
 docker buildx create --name biosimbuilder --use
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64 -t biosimulator-processes . \
+docker buildx build --platform linux/amd64 -t biosimulators/biosimulator-processes:0.0.1 . \
   && docker run --platform linux/amd64 -it -p 8888:8888 biosimulator-processes
 
 
