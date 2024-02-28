a46409bf3a8551799b7a53ddba3ae47a5a9596b9
alexPatrie <alexanderpatrie@gmail.com>
2024-02-28 16:20:47 -0500

    moved generate process instance func to utils

--- biosimulator_processes/utils.py
+++ biosimulator_processes/utils.py
@@ -20,3 +20,40 @@ def fetch_biomodel_by_term(term: str, index: int = 0):
 def fetch_biomodel(model_id: str):
     sbml = biomodels.get_content_for_model(model_id)
     return load_model_from_string(sbml)
+
+
+def generate_copasi_process_instance(config: dict, instance_name: str):
+    return {
+        instance_name: {
+            '_type': 'process',
+            'address': 'local:copasi',
+            'config': config,
+            'inputs': {
+                'floating_species': ['floating_species_store'],
+                'model_parameters': ['model_parameters_store'],
+                'time': ['time_store'],
+                'reactions': ['reactions_store']
+            },
+            'outputs': {
+                'floating_species': ['floating_species_store'],
+                'time': ['time_store'],
+            }
+        },
+        'emitter': {
+            '_type': 'step',
+            'address': 'local:ram-emitter',
+            'config': {
+                'emit': {
+                    'floating_species': 'tree[float]',
+                    'time': 'float',
+                },
+            },
+            'inputs': {
+                'floating_species': ['floating_species_store'],
+                'time': ['time_store'],
+            }
+        }
+    }
+
+
+
