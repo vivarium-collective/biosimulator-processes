74ee25c22f442575466c868058a2b39f0073f2b3
alexPatrie <alexanderpatrie@gmail.com>
2024-03-04 13:27:07 -0500

    updated and added notebook fix to scripts

---
+++ scripts/fix_notebooks.py
@@ -0,0 +1,24 @@
+import nbformat
+import os
+
+
+def fix_execution_count(notebook_path):
+    with open(notebook_path, 'r', encoding='utf-8') as f:
+        nb = nbformat.read(f, as_version=4)
+
+    for cell in nb['cells']:
+        if cell['cell_type'] == 'code':
+            if 'execution_count' not in cell:
+                cell['execution_count'] = None
+            print('execution_count' in cell)
+
+    with open(notebook_path, 'w', encoding='utf-8') as f:
+        nbformat.write(nb, f)
+
+
+if __name__ == '__main__':
+    root = "notebooks"
+    for f in os.listdir(root):
+        fp = os.path.join(root, f)
+        print(fp)
+        fix_execution_count(fp)
