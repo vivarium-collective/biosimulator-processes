525a580a262122972304269ac21d47d3a12bbb27
alexPatrie <alexanderpatrie@gmail.com>
2024-03-05 12:13:41 -0500

    added param scan to timecourse

--- biosimulator_processes/steps/parameter_scan.py
+++ biosimulator_processes/steps/parameter_scan.py
@@ -1,7 +1,40 @@
 from typing import *
 from abc import abstractmethod
 from process_bigraph import Step
+from biosimulator_processes.data_model import TimeCourseProcessConfigSchema
+from biosimulator_processes.processes.copasi_process import CopasiProcess
 
+
+class DeterministicTimeCourseParameterScan(Step):
+    """Using CopasiProcess as the primary TimeCourse simulator.
+
+        # TODO: enable multiple Simulator types.
+    """
+    config_schema = {
+        'process_config': TimeCourseProcessConfigSchema().model_dump(),
+        'n_iterations': 'int',
+        'perturbation_magnitude': 'float',
+        'parameters': 'tree[string]'}
+
+    def __init__(self, config=None):
+        super().__init__(config=config)
+        self.process = CopasiProcess(config=self.config.get('process_config'))
+        self.params_to_scan = self.config.get('parameters')
+
+    def initial_state(self):
+        return self.process.initial_state()
+
+    def inputs(self):
+        return self.process.inputs()
+
+    def outputs(self):
+        return self.process.outputs()
+
+    def update(self, state):
+        """Here is where the method of perturbation differs: deterministic will use
+            `np.linspace(...`
+        """
+        pass
 
 class ParameterScan(Step):
     config_schema = {
@@ -28,7 +61,7 @@ class ParameterScan(Step):
         pass
 
 
-class DeterministicParameterScan(ParameterScan):
+class _DeterministicParameterScan(ParameterScan):
     config_schema = {
         'n_iterations': 'int',
         'perturbation_magnitude': 'float',
