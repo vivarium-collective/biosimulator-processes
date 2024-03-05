86b2b54ded8c0f44564d6e6af5948df8e686aa1e
alexPatrie <alexanderpatrie@gmail.com>
2024-03-05 12:17:13 -0500

    refactored param scan datamodel

--- biosimulator_processes/steps/parameter_scan.py
+++ biosimulator_processes/steps/parameter_scan.py
@@ -5,7 +5,31 @@ from biosimulator_processes.data_model import TimeCourseProcessConfigSchema
 from biosimulator_processes.processes.copasi_process import CopasiProcess
 
 
-class DeterministicTimeCourseParameterScan(Step):
+class ParameterScan(Step):
+    config_schema = {
+        'n_iterations': 'int',
+        'perturbation_magnitude': 'float',
+        'parameters': 'tree[string]',  # ie: 'param_type: param_name'... like 'global': 'ADP', 'species': 'S3'
+        'process_config': 'tree[string]'}  # use the builder to extract this
+
+    @abstractmethod
+    def inputs(self):
+        pass
+
+    @abstractmethod
+    def outputs(self):
+        pass
+
+    @abstractmethod
+    def initial_state(self):
+        pass
+
+    @abstractmethod
+    def update(self, state):
+        pass
+
+
+class DeterministicTimeCourseParameterScan(ParameterScan):
     """Using CopasiProcess as the primary TimeCourse simulator.
 
         # TODO: enable multiple Simulator types.
@@ -36,80 +60,13 @@ class DeterministicTimeCourseParameterScan(Step):
         """
         pass
 
-class ParameterScan(Step):
-    config_schema = {
-        'n_iterations': 'int',
-        'perturbation_magnitude': 'float',
-        'parameters': 'tree[string]',  # ie: 'param_type: param_name'... like 'global': 'ADP', 'species': 'S3'
-        'process_instance': 'object'}  # use the builder to extract this
-        # 'process_instances': 'tree[object]'
-
-    @abstractmethod
-    def inputs(self):
-        pass
-
-    @abstractmethod
-    def outputs(self):
-        pass
-
-    @abstractmethod
-    def initial_state(self):
-        pass
-
-    @abstractmethod
-    def update(self, state):
-        pass
-
-
-class _DeterministicParameterScan(ParameterScan):
-    config_schema = {
-        'n_iterations': 'int',
-        'perturbation_magnitude': 'float',
-        'parameters': 'tree[string]',
-        'process_instance': 'object'}  # use the builder to extract this
-    
-    def __init__(self):
-        super().__init__()
-    
-    @abstractmethod
-    def inputs(self):
-        pass
-
-    @abstractmethod
-    def outputs(self):
-        pass
-
-    @abstractmethod
-    def initial_state(self):
-        pass
-
-    @abstractmethod
-    def update(self, state):
-        pass
-
 
 class StochasticParameterScan(ParameterScan):
+    """Analogous to Monte Carlo perturbations"""
     config_schema = {
         'n_iterations': 'int',
         'perturbation_magnitude': 'float',
         'parameters': 'tree[string]',
-        'process_instance': 'object'}  # use the builder to extract this
+        'process_config': 'tree[string]'}
 
-    def __init__(self):
-        super().__init__()
-
-    @abstractmethod
-    def inputs(self):
-        pass
-
-    @abstractmethod
-    def outputs(self):
-        pass
-
-    @abstractmethod
-    def initial_state(self):
-        pass
-
-    @abstractmethod
-    def update(self, state):
-        pass
+    pass
