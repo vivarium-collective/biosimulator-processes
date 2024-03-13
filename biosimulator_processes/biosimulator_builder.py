c5f5f1ffd5e16c80a8b7a52e3d530b3505fde78f
alexPatrie <alexanderpatrie@gmail.com>
2024-03-13 14:48:27 -0400

    enforce singleton in builder

--- biosimulator_processes/biosimulator_builder.py
+++ biosimulator_processes/biosimulator_builder.py
@@ -4,6 +4,24 @@ from biosimulator_processes import CORE
 
 
 class BiosimulatorBuilder(Builder):
-    def __init__(self, schema: Dict = None, tree: Dict = None, filepath: str = None):
-        super().__init__(schema=schema, tree=tree, file_path=filepath, core=CORE)
+    _instance = None
+    _is_initialized = False
 
+    def __init__(self, schema: Dict = None, tree: Dict = None, filepath: str = None):
+        if not self._is_initialized:
+            super().__init__(schema=schema, tree=tree, file_path=filepath, core=CORE)
+            # Perform your initialization here
+            self._is_initialized = True
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._is_initialized:
+            if not cls._instance:
+                cls._instance = super(BiosimulatorBuilder, cls).__new__(cls, *args, **kwargs)
+            return cls._instance
+        else:
+            raise RuntimeError('Only one instance of this class can be created at a time.')
+
+
+# class BiosimulatorBuilder(Builder):
+#     def __init__(self, schema: Dict = None, tree: Dict = None, filepath: str = None):
+#         super().__init__(schema=schema, tree=tree, file_path=filepath, core=CORE)
