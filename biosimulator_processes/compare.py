dbcacf85c5144f41c368912581c8a2ba8a9e0a4d
alex patrie <alexanderpatrie@gmail.com>
2024-05-01 00:24:57 -0400

    feat: added schemas for handling simulation validation score

--- biosimulator_processes/compare.py
+++ biosimulator_processes/compare.py
@@ -4,7 +4,6 @@ from process_bigraph import Process
 
 def generate_composite_index(results: Dict[str, float]):
     num_population = len(list(results.keys()))
-    print(num_population)
     return sum(list(results.values())) / num_population
 
 
@@ -17,8 +16,10 @@ class ODEComparator(Process):
             '_default': 'deterministic',
             '_type': 'string'
         },
-        'simulators': 'list[string]'
-    }
+        'simulators': 'list[string]',
+        'target_parameter': 'maybe[tree[union[string, float]]]',  # derived from experimental data for fitness calculation
+        'target_dataset': 'maybe[tree[string, tree[union[string, float]]]]'  # possible encapsulating many target_parameters
+        # TODO: are experimental datasets which match the ports available?
 
     def __init__(self, config=None, core=None):
         super().__init__(config, core)
@@ -27,24 +28,24 @@ class ODEComparator(Process):
         self.model_parameters_list = []
         self.reaction_list = []
         self.species_context_key = 'floating_species_concentrations'
-        self.simulator_instances = {}
+        self.simulator_instances = self._set_simulator_instances()
 
+    def _set_simulator_instances(self):
+        simulator_instances = {}
         for simulator in self.config['simulators']:
             module_name = simulator + '_process'
             class_name = simulator.replace(simulator[0], simulator[0].upper()) + 'Process'
             import_statement = f'biosimulator_processes.processes.{module_name}'
             simulator_module = __import__(import_statement, fromlist=[class_name])
             simulator_instance = getattr(simulator_module, class_name)
-            self.simulator_instances[simulator] = simulator_instance
-
-        # TODO: possibly create unique instance params as required by each simulator and instantiate here
+            simulator_instances[simulator] = simulator_instance
+        return simulator_instances
 
     def initial_state(self):
         # TODO: get these values from constructor
         return {
-            simulator: {}
-            for simulator in self.config['simulators']
-        }
+            simulator_name: simulator_process.initial_state()
+            for simulator_name, simulator_process in self.simulator_instances.items()}
 
     def inputs(self):
         # dependent on species context set in self.config
@@ -52,26 +53,33 @@ class ODEComparator(Process):
             species_id: {
                 '_type': 'float',
                 '_apply': 'set'}
-            for species_id in self.floating_species_list
-        }
+            for species_id in self.floating_species_list}
 
         model_params_type = {
             param_id: {
                 '_type': 'float',
                 '_apply': 'set'}
-            for param_id in self.model_parameters_list
-        }
+            for param_id in self.model_parameters_list}
 
         reactions_type = {
             reaction_id: 'float'
-            for reaction_id in self.reaction_list
-        }
+            for reaction_id in self.reaction_list}
 
-        return {
-            'time': 'float',
-            self.species_context_key: floating_species_type,
-            'model_parameters': model_params_type,
-            'reactions': reactions_type}
+        input_schema = {
+            simulator_name: {
+                'time': 'float',
+                self.species_context_key: floating_species_type,
+                'model_parameters': model_params_type,
+                'reactions': reactions_type}
+            for simulator_name in self.simulator_instances.keys()}
+
+        if self.config.get('target_parameter'):
+            for sim in self.simulator_instances.keys():
+                input_schema[sim]['validation_score'] = {
+                    '_type': 'float',
+                    '_apply': 'set'}
+
+        return input_schema
 
     def outputs(self):
         floating_species_type = {
@@ -80,12 +88,35 @@ class ODEComparator(Process):
                 '_apply': 'set'}
             for species_id in self.floating_species_list
         }
-        return {
-            'time': 'float',
-            self.species_context_key: floating_species_type}
+        output_schema = {
+            simulator_name: {
+                'time': 'float',
+                self.species_context_key: floating_species_type
+            } for simulator_name in self.simulator_instances.keys()}
+
+        if self.config.get('target_parameter'):
+            for sim in self.simulator_instances.keys():
+                output_schema[sim]['validation_score'] = {
+                    '_type': 'float',
+                    '_apply': 'set'}
+
+        return output_schema
 
     def update(self, state, interval):
         # TODO: do this in parallel
+        results = {
+            simulator_name: simulator_process.update(state, interval)
+            for simulator_name, simulator_process in self.simulator_instances.items()}
+
+        if self.config.get('target_parameter'):
+            target_param: dict = self.config['target_parameter']
+            for simulator in self.simulator_instances.keys():
+                simulator_values = results[simulator][self.species_context_key]
+                simulator_result_value = simulator_values.get(target_param['name'])
+                diff = target_param['value'] - simulator_result_value
+                results[simulator]['validation_score'] = diff
+
+        return results
 
 
 
