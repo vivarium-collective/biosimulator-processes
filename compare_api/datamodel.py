fe594d136016a10f55e6dbbf18471009571fa05c
alex patrie <alexanderpatrie@gmail.com>
2024-05-01 03:58:52 -0400

    fix: updated base model implementation for datamodel

--- compare_api/datamodel.py
+++ compare_api/datamodel.py
@@ -1,10 +1,14 @@
 from typing import List, Dict, Tuple, Any
-from pydantic import BaseModel
+from pydantic import BaseModel as _Base, ConfigDict
+
+
+class BaseModel(_Base):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
 class SimulatorComparisonResult(BaseModel):
     simulators: List[str]
-    value: Dict[Tuple[str], Dict[Any]]
+    value: Dict[Tuple[str], Dict[str, Any]]
 
 
 class CompositeRunError(BaseModel):
