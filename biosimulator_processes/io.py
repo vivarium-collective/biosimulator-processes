97bc7f032b5b57c3e6816dad66ad93d2b46d4778
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-05-10 13:17:21 -0400

    chore: updated fp name

--- biosimulator_processes/io.py
+++ biosimulator_processes/io.py
@@ -10,12 +10,12 @@ def fetch_sbml_file(biomodel_id: str, save_dir: Optional[str] = None) -> str:
     response = requests.get(url, headers=headers)
     model_filename = f'{biomodel_id}.xml'
     dirpath = save_dir or os.getcwd()
-    response_fp = os.path.join(dirpath, 'results.zip')
-    if not os.path.exists(response_fp):
-        with open(response_fp, 'wb') as f:
+    response_zip_fp = os.path.join(dirpath, 'results.zip')
+    if not os.path.exists(response_zip_fp):
+        with open(response_zip_fp, 'wb') as f:
             f.write(response.content)
 
-        with zf.ZipFile(response_fp, 'r') as zipRef:
+        with zf.ZipFile(response_zip_fp, 'r') as zipRef:
             zipRef.extract(model_filename, dirpath)
 
     return os.path.join(dirpath, model_filename)
