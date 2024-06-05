e4074fc088c6200a0449d81a337388542bf3ab81
alexPatrie <alexanderpatrie@gmail.com>
2024-06-05 18:09:11 -0400

    feat: added sbml scan

--- biosimulator_processes/api/instance.py
+++ biosimulator_processes/api/instance.py
@@ -50,5 +50,111 @@ def generate_ode_instance(process_address: str, model_fp: str, step_size: float,
         duration=duration)
 
 
+class UtcComparisonDocument(dict):
+    def __new__(cls, model_source: str, *args, **kwargs):
+        """Document factory for composite simulations related to the comparison
+            of Uniform Time Course simulator outputs. Inherits from the python built-in `dict`
+            factory.
+        """
+        # TODO: support more config options.
+        utc_comparison_document = {
+            'amici': {
+                '_type': 'step',
+                'address': 'local:utc-amici',
+                'config': {
+                    'model': {
+                        'model_source': model_source
+                    }
+                },
+                'inputs': {
+                    'time': ['time_store'],
+                    'floating_species': ['amici_floating_species_store'],
+                    'model_parameters': ['amici_model_parameters_store'],
+                    'reactions': ['amici_reactions_store']
+                },
+                'outputs': {
+                    'time': ['time_store'],
+                    'floating_species': ['amici_floating_species_store']
+                }
+            },
+            'copasi': {
+                '_type': 'step',
+                'address': 'local:utc-copasi',
+                'config': {
+                    'model': {
+                        'model_source': model_source
+                    }
+                },
+                'inputs': {
+                    'time': ['time_store'],
+                    'floating_species': ['copasi_floating_species_store'],
+                    'model_parameters': ['copasi_model_parameters_store'],
+                    'reactions': ['copasi_reactions_store']
+                },
+                'outputs': {
+                    'time': ['time_store'],
+                    'floating_species': ['copasi_floating_species_store']
+                }
+            },
+            'tellurium': {
+                '_type': 'step',
+                'address': 'local:utc-tellurium',
+                'config': {
+                    'model': {
+                        'model_source': model_source
+                    }
+                },
+                'inputs': {
+                    'time': ['time_store'],
+                    'floating_species': ['tellurium_floating_species_store'],
+                    'model_parameters': ['tellurium_model_parameters_store'],
+                    'reactions': ['tellurium_reactions_store']
+                },
+                'outputs': {
+                    'time': ['time_store'],
+                    'floating_species': ['tellurium_floating_species_store']
+                }
+            },
+            'comparison': {
+                '_type': 'step',
+                'address': 'local:utc-comparator',
+                'config': {
+                    'simulators': ['amici', 'copasi', 'tellurium'],
+                },
+                'inputs': {
+                    'time': ['time_store'],
+                    'amici_floating_species': ['amici_floating_species_store'],
+                    'copasi_floating_species': ['copasi_floating_species_store'],
+                    'tellurium_floating_species': ['tellurium_floating_species_store'],
+                },
+                'outputs': {
+                    'results': ['results_store'],
+                }
+            },
+            'emitter': {
+                '_type': 'step',
+                'address': 'local:ram-emitter',
+                'config': {
+                    'emit': {
+                        'results': 'tree[string]',
+                    }
+                },
+                'inputs': {
+                    'results': ['results_store'],
+                }
+            }
+        }
+
+
+        utc_comparison_output_bridge = {
+            'inputs': {
+                'results': ['results_store']
+            },
+            'outputs': {
+                'results': ['results_store']
+            }
+        }
+
+        return {'state': utc_comparison_document, 'bridge': utc_comparison_output_bridge}
 
 
