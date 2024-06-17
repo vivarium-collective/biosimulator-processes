a8a7a6670081349709ae25e331d003d086b3bfb5
alexPatrie <alexanderpatrie@gmail.com>
2024-06-17 09:44:28 -0400

    feat: updated synapse config of neuron model

--- biosimulator_processes/steps/neuron_network.py
+++ biosimulator_processes/steps/neuron_network.py
@@ -21,15 +21,15 @@ class SimpleNeuronNetwork(Step):
             'param_config': 'tree[string]'
         },
         'synapse_config': 'tree[string]',  # {'synapse_name': {synapse_id: '', synapse_params: {}}}
-        'network_config': {
-            'network_id': 'string',
-            'validate': {
-                '_type': 'boolean',
-                '_default': False
-            },
-            'num_population': 'integer',
-            'population_config': 'tree'  # indexted by population: population size, etc
-        }
+        # 'network_config': {
+        #     'network_id': 'string',
+        #     'validate': {
+        #         '_type': 'boolean',
+        #         '_default': False
+        #     },
+        #     'num_population': 'integer',
+        #     'population_config': 'tree'  # indexted by population: population size, etc
+        # }
     }
 
     def __init__(self, config, core=CORE):
@@ -40,7 +40,7 @@ class SimpleNeuronNetwork(Step):
 
         # declare and configure the entire model
         model_config = self.config.get('doc_config')
-        doc_name = model_config['doc']
+        doc_name = model_config['doc_name']
         doc_id = model_config['doc_id']
 
         model_name = model_config['model_name']  # ie: Cell id like "Izhikevich2007Cell"
@@ -51,7 +51,7 @@ class SimpleNeuronNetwork(Step):
         nml_doc = component_factory(doc_name, id=doc_id)
 
         # add model-specific param state to doc
-        model = nml_doc.add(model_name, model_id, **model_params_config)
+        model = nml_doc.add(model_name, id=model_id, **model_params_config)
         model.info(True)
 
         # create a synapse component and add it to the doc
@@ -60,10 +60,13 @@ class SimpleNeuronNetwork(Step):
         self.synapses = {}
         for i, synapse_config in enumerate(self.config['synapse_config'].items()):
             self.synapses[f'synapse_{i}'] = self._create_synapse(
-                nml_doc=nml_doc, name=synapse_config[0], **synapse_config[1])
+                nml_doc=nml_doc,
+                name=synapse_config[0],
+                synapse_id=synapse_config[1]['synapse_id'],
+                **synapse_config[1]['synapse_params'])
 
-    def _create_synapse(self, nml_doc: object, name: str, syn_id: str, gbase: str, erev: str, tau_decay: str):
-        return nml_doc.add(name, id=syn_id, gbase=gbase, erev=erev, tau_decay=tau_decay)
+    def _create_synapse(self, nml_doc: object, name: str, synapse_id: str, gbase: str, erev: str, tau_decay: str):
+        return nml_doc.add(name, id=synapse_id, gbase=gbase, erev=erev, tau_decay=tau_decay)
 
     def outputs(self):
         return {
