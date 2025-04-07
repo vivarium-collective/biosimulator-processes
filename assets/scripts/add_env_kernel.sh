1e294157197e6d114a0dbc158f45f7adc827f02f
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2025-04-07 16:50:22 -0400

    chore: cleanup of entire repo

--- assets/scripts/add_env_kernel.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-env=$(conda env list | grep '*' | awk '{print $1}')
-python_version=$(conda run -n "$env" python --version)
-conda run -n "$env" python -m ipykernel install --user --name="$env" --display-name "bsp($env): $python_version"
--- backend/common/temporal/__init__.py
+++
@@ -1,5 +0,0 @@
-from biosim_server.common.temporal.converter import pydantic_data_converter
-
-__all__ = [
-    "pydantic_data_converter"
-]
